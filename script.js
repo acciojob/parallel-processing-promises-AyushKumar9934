37e6c459ba66e13e7f7426815962eed361db8081
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-17 17:47:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function download(images) {
+  
+ let promise= images.map((item, i) => {
+  return new Promise((resolve,reject)=>{
+    fetch(item.url).then(res=>{
+      if(!res.ok){
+        throw new Error(`Failed to load image's URL: ${item.url}`)
+        
+      }
+      else return res.blob();
+    }).then(image=>{
+      let imageURL=URL.createObjectURL(image);
+      resolve(imageURL);
+    }).catch((error)=>{
+      reject(error);
+    })
+  })
+  })
+  Promise.all(promise).then((data)=>{
+    data.map((url)=>{
+      let img = document.createElement('img');
+        img.src = url;
+        output.appendChild(img);
+
+    })
+  }).catch((error)=>console.log(error))
+    
+}
+
+document.getElementById("download-images-button").addEventListener("click",()=>download(images))
